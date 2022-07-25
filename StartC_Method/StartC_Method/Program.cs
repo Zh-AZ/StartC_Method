06b1c794e9ba8b7a6e89b84ee1f4f1d5f92cd2b5
Zh-AZ <zhaparovaziz03@gmail.com>
2022-07-25 18:26:28 +0300

    StartC_Method

---
+++ StartC_Method/StartC_Method/Program.cs
@@ -0,0 +1,41 @@
+﻿static string[] UserSeparator()
+{
+    Console.Write("Введите текст: ");
+    string console = Console.ReadLine();
+    string[] inputed = console.Split(' ');
+    return inputed;
+}
+
+static void UserConclusion()
+{
+    foreach (var sub in UserSeparator())
+    {
+        Console.WriteLine(sub);
+    }
+}
+UserConclusion();
+Console.ReadKey();
+
+
+
+Console.WriteLine("Введите предложение: ");
+string consol = Console.ReadLine();
+
+static string[] ReverseWord(string[] user_words)
+{
+    Array.Reverse(user_words);
+    return user_words;
+}
+
+static string[] SplitWord(string split)
+{
+    var splitword = split.Split(" ");
+    return splitword;
+}
+
+string[] user = ReverseWord(SplitWord(consol));
+foreach (var tum in user)
+{
+    Console.WriteLine($"{tum}");
+}
+Console.ReadKey();
